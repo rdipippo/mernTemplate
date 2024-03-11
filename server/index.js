6ce47a135bcd67058b81d6b1a72a600bb4f9a683
Richard DiPippo <rdipippo@Richards-iMac.lan>
2024-03-11 10:46:13 -0400

    First commit

---
+++ server/index.js
@@ -0,0 +1,19 @@
+
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/health', (req, res) => {
+   res.json({status: 'OK'});
+});
+
+const PORT = process.env.PORT || 3001;
+
+const server = app.listen(PORT, () => {
+  console.log("server is running on port", server.address().port);
+});
--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import {useEffect, useState} from 'react';
 
 function App() {
+  const [status, setStatus] = useState("");
+
+  useEffect(() => {
+
+    axios.get("http://192.168.40.100:3001/health").then(response => {
+      setStatus(response.data.status);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          Server healthcheck: {status}
         </p>
         <a
           className="App-link"
